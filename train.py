47e59d16afba1bffe57f5eed3cb9baa0701259ed
= <huydao98.uet@gmail.com>
2020-05-12 13:10:48 +0700

    fix paramters

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
 
 
     optimizer = optim.Adam(model.parameters(), lr = args.lr)
-    lr_scheduler = optim.lr_scheduler.CyclicLR(optimizer = optimizer,base_lr=args.lr,max_lr=args.max_lr,step_size_up=2000)
+    lr_scheduler = optim.lr_scheduler.CyclicLR(optimizer = optimizer,base_lr=args.lr,max_lr=args.max_lr,step_size_up=2000,cycle_momentum= True)
 
     trainer = DogCatTrainer(model= model,
                             optimizer= optimizer,
