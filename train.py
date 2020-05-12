ca5fc1fba81ea60fb60d011ec8b17cc2135f1a26
= <huydao98.uet@gmail.com>
2020-05-12 11:44:15 +0700

    fix variable

--- train.py
+++ train.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
 
 
     criterion = nn.CrossEntropyLoss()
-    metric = ClassificationMetric(n_classes=args.n_classes)
+    # metric = ClassificationMetric(n_classes=args.n_classes)
 
     writer = Writer(log_dir=args.log_dir)
 
